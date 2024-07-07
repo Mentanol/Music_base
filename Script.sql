a630982ce7e62003c96f60379101285e4ff48861
Mentanol <142655318+Mentanol@users.noreply.github.com>
2024-07-07 16:42:06 +0800

    Update Script.sql

--- Script.sql
+++ Script.sql
@@ -1,10 +1,12 @@
 
-DROP TABLE IF EXISTS playlists CASCADE;
+DROP TABLE IF EXISTS artist_genre CASCADE;
+DROP TABLE IF EXISTS artist_album CASCADE;
+DROP TABLE IF EXISTS playlist_track CASCADE;
 DROP TABLE IF EXISTS tracks CASCADE;
 DROP TABLE IF EXISTS albums CASCADE;
 DROP TABLE IF EXISTS artists CASCADE;
 DROP TABLE IF EXISTS genres CASCADE;
-DROP TABLE IF EXISTS playlists_track CASCADE;
+DROP TABLE IF EXISTS playlists CASCADE;
 
 
 CREATE TABLE genres (
@@ -12,42 +14,55 @@ CREATE TABLE genres (
     name VARCHAR(100) NOT NULL
 );
 
+
 CREATE TABLE artists (
     id INT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    genres_id INT,
-    FOREIGN KEY (genres_id) REFERENCES genres(id)
+    name VARCHAR(100) NOT NULL
 );
 
 
 CREATE TABLE albums (
     id INT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    release_year INT CHECK (release_year BETWEEN 1000 AND 9999),
+    release_year INT CHECK (release_year BETWEEN 1000 AND 9999)
+);
+
+CREATE TABLE artist_album (
     artist_id INT,
-    FOREIGN KEY (artist_id) REFERENCES artists(id)
+    album_id INT,
+    FOREIGN KEY (artist_id) REFERENCES artists(id),
+    FOREIGN KEY (album_id) REFERENCES albums(id),
+    PRIMARY KEY (artist_id, album_id)
+);
+
+CREATE TABLE artist_genre (
+    artist_id INT,
+    genre_id INT,
+    FOREIGN KEY (artist_id) REFERENCES artists(id),
+    FOREIGN KEY (genre_id) REFERENCES genres(id),
+    PRIMARY KEY (artist_id, genre_id)
 );
 
 CREATE TABLE tracks (
     id INT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
-    duration INT,
+    duration DECIMAL,
     album_id INT,
     FOREIGN KEY (album_id) REFERENCES albums(id)
 );
 
-create table playlist (
-	id INT primary key ,
-	titile varchar(50) not null,
-	release_year INT CHECK (release_year BETWEEN 1000 AND 9999)
+
+CREATE TABLE playlists (
+    id INT PRIMARY KEY,
+    name VARCHAR(50),
+    release_year INT CHECK (release_year BETWEEN 1000 AND 9999)
 );
 
 
-
-CREATE TABLE playlists_track (
+CREATE TABLE playlist_track (
     playlist_id INT,
     track_id INT,
-    FOREIGN KEY (playlist_id) REFERENCES playlist(id),
-    FOREIGN KEY (track_id) REFERENCES tracks(id)
-    primary key (playlist_id, track_id)
+    FOREIGN KEY (playlist_id) REFERENCES playlists(id),
+    FOREIGN KEY (track_id) REFERENCES tracks(id),
+    PRIMARY KEY (playlist_id, track_id)
 );
